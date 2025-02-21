b437f70f87b5859744816956813bca2e8a6aeae4
kimjaehee <dkdlwpa212@gmail.com>
2025-02-21 14:10:23 +0900

    UI_PR

---
+++ Plugins/DBDUI/Source/DBDUI/Public/IngamePlayerInfoImageWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DBDUserWidget.h"
+#include "IngamePlayerInfoImageWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DBDUI_API UIngamePlayerInfoImageWidget : public UDBDUserWidget
+{
+	GENERATED_BODY()
+	
+};
