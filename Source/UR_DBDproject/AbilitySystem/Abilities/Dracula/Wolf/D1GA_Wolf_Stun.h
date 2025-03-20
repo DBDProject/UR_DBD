eb835e4004d30fde3bc85c1bc16cd9040df77f5e
wite0927 <wite0927@gmail.com>
2025-03-20 16:41:58 +0900

    Wolf Stun
    Wolf Stun

--- Source/UR_DBDproject/AbilitySystem/Abilities/Dracula/Wolf/D1GA_Wolf_Stun.h
+++ Source/UR_DBDproject/AbilitySystem/Abilities/Dracula/Wolf/D1GA_Wolf_Stun.h
@@ -14,4 +14,37 @@ class UR_DBDPROJECT_API UD1GA_Wolf_Stun : public UD1GameplayAbility
 {
 	GENERATED_BODY()
 	
+public:
+	UD1GA_Wolf_Stun(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	virtual bool CanActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayTagContainer* SourceTags,
+		const FGameplayTagContainer* TargetTags,
+		FGameplayTagContainer* OptionalRelevantTags) const override;
+
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData) override;
+
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled) override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TObjectPtr<class UAnimMontage> Wolf_Stun;
+
+private:
+	void OnEndMontage(UAnimMontage* Montage, bool bInterrupted);
+
+public:
+	UFUNCTION(NetMulticast, Reliable)
+	void Multicast_WolfStun(AD1KillerBase* Player);
 };
