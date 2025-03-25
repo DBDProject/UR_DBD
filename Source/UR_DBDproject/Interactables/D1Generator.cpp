b5095baaee6e91361f101a41bd25713fa9bc8df2
kimjaehee <dkdlwpa212@gmail.com>
2025-03-25 10:17:41 +0900

    KJH_UI_Server_2

--- Source/UR_DBDproject/Interactables/D1Generator.cpp
+++ Source/UR_DBDproject/Interactables/D1Generator.cpp
@@ -327,7 +327,10 @@ void AD1Generator::CompleteRepair()
 
     StopRepairAll();
 
-    BP_CompleteRepair();
+    if (HasAuthority())
+    {
+        BP_CompleteRepair();
+    }
 
     UE_LOG(LogTemp, Warning, TEXT("발전기 수리 완료! 모든 플레이어에게 알림"));
     if (AD1GameState* GameState = GetWorld()->GetGameState<AD1GameState>())
